b0971a693890b37845298fddc3467b2a8f6089ac
valeeerio <valeriomortella03@gmail.com>
2024-05-03 16:27:37 +0200

    fix second page

--- app/registrati/page.tsx
+++ app/registrati/page.tsx
@@ -19,48 +19,40 @@ export default function Page() {
             </Button>
           </div>
 
-          <div className="flex flex-1 flex-col mt-8 mr-8 ml-8">
-            <h1 className="m-4 font-bold text-lg">Registrati</h1>
+          <div className="flex flex-col">
+            <div className="flex flex-1 flex-col mt-8 mr-8 ml-8">
+              <h1 className="m-4 font-bold text-lg">Registrati</h1>
 
-            <div className="flex items-center justify-between my-3">
-              Data di nascita
-              <Input type="date" className="w-48" />
+              <div className="flex items-center justify-between my-3">
+                Nome
+                <Input type="text" placeholder="Nome" className="w-48" />
+              </div>
+              <div className="flex items-center justify-between my-3">
+                Cognome
+                <Input type="text" placeholder="Cognome" className="w-48" />
+              </div>
+              <div className="flex items-center justify-between my-3">
+                Username
+                <Input type="text" placeholder="Username" className="w-48" />
+              </div>
+              <div className="flex items-center justify-between my-3">
+                Email
+                <Input type="email" placeholder="Email" className="w-48" />
+              </div>
+              <div className="flex items-center justify-between my-3">
+                Password
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  className="w-48"
+                />
+              </div>
             </div>
-            <div className="flex items-center justify-between my-3">
-              Città
-              <Input type="text" placeholder="Città" className="w-48" />
-            </div>
-            <div className="flex items-center justify-between my-3">
-              Email
-              <Input type="email" placeholder="Email" className="w-48" />
-            </div>
-            <div className="flex items-center justify-between my-3">
-              Nome
-              <Input type="text" placeholder="Nome" className="w-48" />
-            </div>
-            <div className="flex items-center justify-between my-3">
-              Organizzazione
-              <Input
-                type="text"
-                placeholder="Organizzazione"
-                className="w-48"
-              />
-            </div>
-            <div className="flex items-center justify-between my-3">
-              Password
-              <Input type="password" placeholder="Password" className="w-48" />
-            </div>
-            <div className="flex items-center justify-between my-3">
-              Ruolo
-              <Input type="text" placeholder="Ruolo" className="w-48" />
-            </div>
-            <div className="flex items-center justify-between my-3">
-              Cognome
-              <Input type="text" placeholder="Cognome" className="w-48" />
-            </div>
-            <div className="flex items-center justify-between my-3">
-              Username
-              <Input type="text" placeholder="Username" className="w-48" />
+
+            <div className="flex justify-center mt-8">
+              <Button variant="secondary" className="w-64">
+                Registrati
+              </Button>
             </div>
           </div>
         </div>
